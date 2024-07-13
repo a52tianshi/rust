42029308110e0fc3d450a826024199ddb617396e
Penny Hardaway <475215536@qq.com>
2024-07-13 23:03:41 +0800

    Update main.rs

--- src/number_theory/hensel_lemma/main.rs
+++ src/number_theory/hensel_lemma/main.rs
@@ -6,14 +6,14 @@ fn main() {
 const MOD_BASE: i64 = 8;
 
 //[7,2,1] : 7*x^2 + 2*x + 1
-static POLYNOMIAL_COEFFICIENTS: [i64; 3] = [0,2,1];
+static POLYNOMIAL_COEFFICIENTS: [i64; 3] = [0, 2, 1];
 
 fn trival_solve() {
     for x in 0..MOD_BASE {
-        let result: i64 = 0;
-        let highestC:u32 = u32(POLYNOMIAL_COEFFICIENTS.len() - 1);
+        let mut result: i64 = 0;
+        let highest_c: u32 = (POLYNOMIAL_COEFFICIENTS.len() - 1) as u32;
         for j in 0..POLYNOMIAL_COEFFICIENTS.len() {
-            result += POLYNOMIAL_COEFFICIENTS[j] * x.pow(highestC - j as u32);
+            result += POLYNOMIAL_COEFFICIENTS[j] * x.pow(highest_c - j as u32);
         }
         let y = result % MOD_BASE;
         println!("x = {}, y = {}", x, y);
